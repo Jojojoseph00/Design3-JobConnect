f1568278c77c59678ce397fdd2886acd35c4868d
Rob <robertdaniel.cook@my.jcu.edu.au>
2018-12-11 15:24:21 +0800

    changed the app component

---
+++ src/app/jobpost/jobpost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobpost',
+  templateUrl: './jobpost.component.html',
+  styleUrls: ['./jobpost.component.css']
+})
+export class JobpostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
